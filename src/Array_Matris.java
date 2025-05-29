55865f467f2fb80b8ee9cb8b66a7463c8e638956
Abdullah Bosut <bosut52@gmail.com>
2025-05-29 20:10:41 +0300

    matris3

---
+++ src/Array_Matris.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Array_Matris {
+    public static void main(String[] args) {
+        int[][] matris = {{2, 3, 4},
+                          {5, 6, 4}};
+
+        int[][] transpozeMatris= new int[matris[0].length][matris.length];
+
+        for(int i=0; i<matris[0].length;i++){
+            for(int j=0; j<matris.length;j++){
+                transpozeMatris[i][j]=matris[j][i];
+            }
+        }
+
+        System.out.println("Transpoze matris: ");
+
+        for(int i = 0; i < transpozeMatris.length; i++){
+            for(int j = 0; j < transpozeMatris[i].length; j++){
+                System.out.print(transpozeMatris[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
